1a584322046b85038ad7c7809661390651eaa15c
Brian Obot <brianobot@gmail.com>
2025-08-23 16:32:17 +0100

    Update lesson on arrays and formatting

--- src/bin/arrays.rs
+++ src/bin/arrays.rs
@@ -2,7 +2,17 @@
 fn main() {
     // arrays can be initialized with shorthand syntax [<initil_value_to_be_repeated>; <number of time to repeat>]
     let zeros = [0; 100];
-
     println!("Zeros: {:?}", zeros);
+
+    // the type annotation of an array includes the number of elements
+    let _ones: [i32; 5] = [1, 1, 1, 1, 1]; // [T; N]
+    // because of this, two arrays with different N and the same T are two different types
+
+    let mut a = [1, 2, 3, 3, 4];
+    // a[6] = 0; // this would cause a compilation error, because a value is assigned outside the bounds of the array
+
+    // println!("a: {:?}", a);
+    
+    // arrays are not heap allocated, their size is known and they go on the stack
 
 }
---
+++ src/bin/formatting.rs
@@ -0,0 +1,37 @@
+fn main() {
+    println!("{1} {} {0} {}", 1, 2);
+    // here the format string first filled the first place {1} with the value @ index 1 which is 2
+    //  then for the second placeholder {} it takes the first item in the iterators of arguments which is 1
+    // then for the third, it uses positional check again like in the first and gets the item at the oth index which is 1
+    // for the forth, it takes the next item in the iterator of argument, which is 2, 
+    // this means that if we provide more arguments that the string consumes, it would fail
+    // this samples would fail
+    // println!("{}{}{}", 1, 2, 3, 4);
+    // println!("{}{}", 1);
+
+    println!("{:.*}", 2, 2.326);
+
+    // when using the : to format the type of an argument the character after : determines how the argument
+    // would be formatted
+    // nothing means it should use the Display trait to format the argument
+    // :.* float trait
+    // :? Debug
+    // :o Octal
+    // :x lower Hex
+    // :X UPPER Hex
+    // :p Pointer
+    // :b Binary
+    // :e lower EXPONENT
+    // :E UPPER EXPONENT
+
+    // what this means that any type that implements that traits for a particular formatting can be used in that formatting
+    println!("Binary 10: {:b}", 10);
+    println!("Hexdecimal 10: {:X}", 10);
+    println!("Upper Exponent: {:E}", 100000);
+    println!("Lower Exponent: {:e}", 100000);
+
+    // adding # in front of the format argument adds pretty printing to the formater
+    println!("Binaary 11: {:#b}", 11);
+    println!("Hexadecimal 12: {:#x}", 12);
+
+}
