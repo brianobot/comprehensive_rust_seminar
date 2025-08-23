cc177d76b31edfcb148e5b310ead470c8937941d
Brian Obot <brianobot@gmail.com>
2025-08-23 17:05:03 +0100

    Update lesson on pattern matching

--- src/bin/04_pattern_matching_and_destructuring.rs
+++ src/bin/04_pattern_matching_and_destructuring.rs
@@ -6,4 +6,8 @@ fn main() {
     let (a, b, c) = tuple;
     println!("a: {}, b: {}, c: {}", a, b, c);
     println!("TUPLE: {tuple:?}");
+
+    // irrefutable patterns are patterns that rust compiler can statically proof that the lhs matches the rhs in terms of their structure
+    // interestingly, a variable name is an irrefutable pattern that match any any value
+    // that's why we can assigned any structure to a variable name with let
 }
