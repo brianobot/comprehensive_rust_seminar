7411e3e0d73e81a9d4fd868f48afc5460a1a97ec
Brian Obot <brianobot@gmail.com>
2025-08-23 16:37:04 +0100

    Add note on tuples

---
+++ src/bin/02_tuples.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    let t = (7, false);
+    dbg!(t.0);
+    dbg!(t.1);
+    // likes arrays tuple have fixed lengths
+    // tuples group values of different type into a compound
+    // fields of a tuple can be access with a dot and the index of the value
+    // () the empty tuple is a special case call the unit type and represent the absense of a value
+    // functions without any return values implicitly return the ()
+    return ()
+}
