a2b91365c631ad0ecf5fd57b83f224ece2fe6dbe
Brian Obot <brianobot@gmail.com>
2025-08-23 16:53:52 +0100

    Update lesson on array iteration

--- src/bin/03_array_iterations.rs
+++ src/bin/03_array_iterations.rs
@@ -1,3 +1,12 @@
 fn main() {
+    // iterations works on array by default
+    // but it does not work on tuples, 
+    // this process is enabled by the IntoIteration Trait, we would get to that soon
     let primes = [2, 3, 5, 7, 11, 13, 17, 19];
+    for prime in primes {
+        for i in 2..prime {
+            assert_ne!(prime % i, 0);
+            println!("prime: {}, i: {}", prime, i);
+        }
+    }
 }
